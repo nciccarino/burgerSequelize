08331fc3d744e746de98bd01bcc4f9c9fc26577e
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-07-06 23:00:47 -0400

    some progress

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require("express"); 
-var methodOverride = require("method-override");
 var bodyParser = require("body-parser"); 
+var methodOverride = require("method-override");
 var path = require("path"); 
+// var mysql = require("mysql"); 
+// var mysql2 = require("mysql2");
+// var fs = require("fs");
 
 var PORT = process.env.PORT || 3000; 
 var app = express(); 
